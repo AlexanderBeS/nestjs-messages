4167a736901437773b15d8b5892c5d1a6e38a9dc
Alexander Bespalov <alexander.bespalov@clockwise.software>
2021-10-25 13:27:08 +0300

    Added DI

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './controllers/messages.controller';
+import { MessagesRepository} from "./messages.repository";
+import { MessagesService } from "./messages.service";
 
 @Module({
   controllers: [MessagesController],
+  providers: [MessagesService, MessagesRepository]
 })
 export class MessagesModule {}
